94b429e32439308a3840afb65ab3345ee47bc9d3
Stefan Eike <stefan.eike@mailbox.org>
2016-11-14 09:35:53 +0100

    Integrate Travis CI

---
+++ travis.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+zip -r org.doctales.javaProperties2Dita.zip . -x *.zip* *.git/* *temp/* *out/*
+curl -LO https://github.com/dita-ot/dita-ot/releases/download/2.3.3/dita-ot-2.3.3.zip
+unzip -q dita-ot-2.3.3.zip
+mv dita-ot-*/ dita-ot/
+chmod +x dita-ot/bin/ant
+dita-ot/bin/ant -buildfile samples/build.xml
